cac5438d2ee343cff7203992261ea1bbd120aed2
TannerRobison <Robison.tnr@gmail.com>
2025-07-18 23:52:26 -0700

    Works Now :)
    Switched to using system Interrupts, Had to switch pinouts to GPIO pin numbers
    
    It works good, tho still a little choppy, trying to figure out how to get it to work with floats for the scaled input to get more numbers, At the moment for some reason it only goes up every 20 counts for the userInput (so only 5 diff brightness levels)

--- LightDock_Prototype/LightDock_Prototype.ino
+++ LightDock_Prototype/LightDock_Prototype.ino
@@ -3,58 +3,66 @@
 int userInput = 0;
 int scaledInput = 0;
 int lightValue = 0; 
-const int pwmPin = 4;
-const int encodePin1 = 8;
-const int encodePin2 = 9;
+const int pwmPin = D4;
+const int encodePin1 = D8;
+const int encodePin2 = D9;
 const int upperLimit = 100;
 //These Two variables are for debouncing
 int currentMillis = 0; 
 int previousMillis = 0;
-const int pollingInterval = 50;
-const int increment = 4;
+const int pollingInterval = 10;
+const int increment = 5;
 
-void setup() {
- pinMode(pwmPin, OUTPUT);
- 
- Serial.begin(115200);
- Serial.print("Booting up...");
-
- pinMode(encodePin1, INPUT);
- pinMode(encodePin2, INPUT);
-}
-
-void loop() {
-  //determines user input
-  
+void encoderMoved() {
+  ///This Function is called when the encoder is moved
   currentMillis = millis();
-  
-  if ((currentMillis - previousMillis) >= pollingInterval) {
-    if (digitalRead(encodePin1) == 0 && digitalRead(encodePin2) == 1) {
-      if (userInput > 0) userInput = userInput - increment;
-      //if (userInput > upperLimit) userInput = upperLimit;
+  if ((currentMillis - previousMillis) > pollingInterval) { 
+    if (digitalRead(encodePin2) == 1) { //moved left
+      if (userInput > 0) userInput = userInput - (increment*2);
       if (userInput < 0) userInput = 0;
-      }
-    if (digitalRead(encodePin1) == 1 && digitalRead(encodePin2) == 0) {
+    }
+    
+    if (digitalRead(encodePin2) == 0) { //moved right
       if (userInput < upperLimit) userInput = userInput + increment;
       if (userInput > upperLimit) userInput = upperLimit;
     }
-    previousMillis = millis();
+
+    //puts input on an exponential scale
+    scaledInput = pow(3, userInput / 20);
+
+    //maps scaled Input range to pwm signal values
+    lightValue = map(scaledInput, 0,  243, 0, 255); //243 because 3^(x/20) = 243
+    //if (scaledInput == 243) lightValue = 255;
+    //else lightValue = (int)scaledInput;
+    
+    if (userInput == 0) lightValue = 0;
+    analogWrite(pwmPin, lightValue); //sends signal to LED bulb
+    
+    //Serial monitoring
+    Serial.print("\nuser input: ");
+    Serial.print(userInput);
+  
+    Serial.print("  Scaled input: ");
+    Serial.print(scaledInput);
+    
+    Serial.print("  light Value: ");
+    Serial.print(lightValue);
+
+    previousMillis = currentMillis;
   }
-  //puts input on an exponential scale
-  if (userInput > 0) scaledInput = pow(3, userInput / 20);
-  if (userInput == 0) scaledInput = 0;
+}
 
-  //maps scaled Input range to pwm signal values
-  lightValue = map(scaledInput, 0,  243, 0, 255); //243 because 3^(x/20) = 243
-  analogWrite(pwmPin, lightValue); //sends signal to LED bulb
-  
-  //Serial monitoring
-  Serial.print("\nuser input: ");
-  Serial.print(userInput);
+void setup() {
+ Serial.begin(115200);
+ Serial.print("Booting up...");
 
-  Serial.print("  Scaled input: ");
-  Serial.print(scaledInput);
+ pinMode(pwmPin, OUTPUT);
+ pinMode(encodePin1, INPUT_PULLUP);
+ pinMode(encodePin2, INPUT_PULLUP);
+
+ attachInterrupt(digitalPinToInterrupt(encodePin1), encoderMoved, FALLING);
+}
+
+void loop() {
   
-  Serial.print("  light Value: ");
-  Serial.print(lightValue);
 }
